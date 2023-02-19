2d9d13e7fadab3e61b2d040c578d837505911510
luvgameon <luvgameon@gmail.com>
2023-02-19 18:25:16 +0530

    Seach added

--- mongoose/index.js
+++ mongoose/index.js
@@ -42,4 +42,20 @@ app.put('/put/:_id',async(req,res)=>{
     
 })
 
+
+//---------------------------------------Serach Method---------------------------------------------------------->
+
+app.get("/search/:key",async (req,resp)=>{
+    let data = await Product.find(
+        {
+            "$or":[
+                {"name":{$regex:req.params.key}},
+                {"company":{$regex:req.params.key}}
+            ]
+        }
+    )
+    resp.send(data);
+
+})
+
 app.listen(500);
