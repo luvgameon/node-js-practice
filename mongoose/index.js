aff90221de93d365cbbe8c05bf7e5597f15e3ae4
luvgameon <luvgameon@gmail.com>
2023-02-19 17:58:20 +0530

    get put delete Updated

--- mongoose/index.js
+++ mongoose/index.js
@@ -3,6 +3,7 @@ require('./config');
 const Product=require('./productsModel');
 const app= express();
 app.use(express.json());
+//---------------------------------------------POST Method--------------------------------------------------->
 app.post('/create',async (req,res)=>{
     const data = new Product(req.body);
     let result= await data.save();
@@ -10,4 +11,35 @@ app.post('/create',async (req,res)=>{
  console.log(result);
 
 });
+
+//---------------------------------------GET Method---------------------------------------------------------->
+
+app.get('/list',async(req,res)=>{
+    let data=await Product.find();
+    console.log(data);
+    res.send(data);
+})
+
+
+
+//---------------------------------------DELETE Method---------------------------------------------------------->
+
+app.delete('/delete/:_id',async(req,res)=>{
+    let data=await Product.deleteOne(req.params);
+    console.log(data);
+    res.send(data);
+})
+
+//---------------------------------------UPDATE Method---------------------------------------------------------->
+
+app.put('/put/:_id',async(req,res)=>{
+    let data=await Product.updateOne(
+        req.params,
+        {$set:req.body}
+        );
+        console.log(data);
+        res.send(data)
+    
+})
+
 app.listen(500);
